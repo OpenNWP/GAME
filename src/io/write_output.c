4cac9d5a4b3fe3ea197857d07272ebc0926db14a
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-07-18 18:41:36 +0200

    header file included

--- src/io/write_output.c
+++ src/io/write_output.c
@@ -9,9 +9,10 @@ In addition to that, some postprocessing diagnostics are also calculated here.
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <netcdf.h>cc
+#include <netcdf.h>
 #include "../game_types.h"
 #include "../game_constants.h"
 #include "io.h"
