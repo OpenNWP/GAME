2fc98bf1d722ee5734fb4e97838b87a93dfe6dd4
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-07-30 19:18:52 +0200

    formal changes

--- src/constants.f90
+++ src/constants.f90
@@ -12,13 +12,14 @@ module constants
   public
   
   ! physical constants
-  real(c_double) :: r_e = 6371000.789927      ! Earth radius
-  real(c_double) :: k_b = 1.380649e-23       ! Boltzmann's constant
-  real(c_double) :: n_a =  6.02214076e23     ! Avogadro's number
-  real(c_double) :: t_0 = 273.15             ! 273.15 K
-  real(c_double) :: rho_h2o = 1024.    ! typical density of water
+  ! ------------------
+  real(c_double) :: r_e = 6371000.789927       ! Earth radius
+  real(c_double) :: k_b = 1.380649e-23         ! Boltzmann's constant
+  real(c_double) :: n_a =  6.02214076e23       ! Avogadro's number
+  real(c_double) :: t_0 = 273.15               ! 273.15 K
+  real(c_double) :: rho_h2o = 1024.            ! typical density of water
   real(c_double) :: p_0 = 100000._c_double     ! reference pressure
-  real(c_double) :: omega = 7.292115e-5      ! angular frequency of Earth rotation
+  real(c_double) :: omega = 7.292115e-5        ! angular frequency of Earth rotation
   real(c_double) :: gravity = 9.80616_c_double ! average surface gravity value
   real(c_double) :: r_d = 287.057811_c_double  ! specific gas constant of dry air
   real(c_double) :: c_d_p = 1005._c_double     ! isobaric specific heat capacity of dry air
@@ -30,12 +31,12 @@ module constants
   
   ! some properties of the standard atmosphere
   ! ------------------------------------------
-  real(c_double) :: lapse_rate = 0.0065_c_double      ! lapse_rate within the troposphere
-  real(c_double) :: surface_temp = 288.15_c_double    ! the temperature at the surface
-  real(c_double) :: tropo_height = 11000._c_double    ! the tropopause height
-  real(c_double) :: inv_height = 20000._c_double      ! height where the temperature inversion begins
-  real(c_double) :: t_grad_inv = 0.001_c_double       ! temperature gradient above the inversion
-  real(c_double) :: p_0_standard = 101325._c_double   ! reference pressure of the standard atmosphere
+  real(c_double) :: lapse_rate = 0.0065_c_double    ! lapse_rate within the troposphere
+  real(c_double) :: surface_temp = 288.15_c_double  ! the temperature at the surface
+  real(c_double) :: tropo_height = 11000._c_double  ! the tropopause height
+  real(c_double) :: inv_height = 20000._c_double    ! height where the temperature inversion begins
+  real(c_double) :: t_grad_inv = 0.001_c_double     ! temperature gradient above the inversion
+  real(c_double) :: p_0_standard = 101325._c_double ! reference pressure of the standard atmosphere
 
 end module constants
 
