ca47d6122a9ccf561681ffe96ec9a24996dd58a1
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-07-31 15:41:08 +0200

    constants simplified

--- src/constants.f90
+++ src/constants.f90
@@ -3,10 +3,9 @@
 
 module constants
 
-  use iso_c_binding
-  use definitions, only: wp
-  
   ! This is a collection of some quantities that are hardly ever changed.
+  
+  use definitions, only: wp
   
   implicit none
   
@@ -14,21 +13,27 @@ module constants
   
   ! physical constants
   ! ------------------
-  real(wp), parameter :: r_e = 6371000.789927_wp ! Earth radius
-  real(wp), parameter :: k_b = 1.380649e-23_wp   ! Boltzmann's constant
-  real(wp), parameter :: n_a =  6.02214076e23_wp ! Avogadro's number
-  real(wp), parameter :: t_0 = 273.15_wp         ! 273.15 K
-  real(wp), parameter :: rho_h2o = 1024._wp      ! typical density of water
-  real(wp), parameter :: p_0 = 100000._wp        ! reference pressure
-  real(wp), parameter :: omega = 7.292115e-5     ! angular frequency of Earth rotation
-  real(wp), parameter :: gravity = 9.80616_wp    ! average surface gravity value
-  real(wp), parameter :: r_d = 287.057811_wp     ! specific gas constant of dry air
-  real(wp), parameter :: c_d_p = 1005._wp        ! isobaric specific heat capacity of dry air
+  real(wp), parameter :: r_e = 6371000.789927_wp   ! Earth radius
+  real(wp), parameter :: k_b = 1.380649e-23_wp     ! Boltzmann's constant
+  real(wp), parameter :: n_a =  6.02214076e23_wp   ! Avogadro's number
+  real(wp), parameter :: t_0 = 273.15_wp           ! 273.15 K
+  real(wp), parameter :: rho_h2o = 1024._wp        ! typical density of water
+  real(wp), parameter :: p_0 = 100000._wp          ! reference pressure
+  real(wp), parameter :: omega = 7.292115e-5_wp    ! angular frequency of Earth rotation
+  real(wp), parameter :: gravity = 9.80616_wp      ! average surface gravity value
+  real(wp), parameter :: r_d = 287.057811_wp       ! specific gas constant of dry air
+  real(wp), parameter :: r_v = 461.524879_wp       ! specific gas constant of water vapour
+  real(wp), parameter :: m_d = n_a*0.004810e-23_wp ! molar mass of dry air
+  real(wp), parameter :: m_v = n_a*0.002991e-23_wp ! molar mass of water
+  real(wp), parameter :: c_d_v = 717.942189_wp     ! isochoric specific heat capacity of dry air
+  real(wp), parameter :: c_v_v = 1396.475121_wp    ! isochoric specific heat capacity of water vapour
+  real(wp), parameter :: c_d_p = 1005._wp          ! isobaric specific heat capacity of dry air
+  real(wp), parameter :: c_v_p = 1858._wp          ! isobaric specific heat capacity of water vapour
   
   ! non-physical constants
   ! ----------------------
-  real(wp), parameter :: M_PI = 4._wp*atan(1._wp) ! pi
-  real(wp), parameter :: EPSILON_SECURITY = 1e-10 ! security constant
+  real(wp), parameter :: M_PI = 4._wp*atan(1._wp)    ! pi
+  real(wp), parameter :: EPSILON_SECURITY = 1e-10_wp ! security constant
   
   ! some properties of the standard atmosphere
   ! ------------------------------------------
@@ -40,7 +45,6 @@ module constants
   real(wp), parameter :: p_0_standard = 101325._wp ! reference pressure of the standard atmosphere
 
 end module constants
-
 
 
 
