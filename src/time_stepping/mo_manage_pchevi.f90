efad99860d21c307711ca9a147313e0635f6e534
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-08-21 23:33:44 +0200

    formal change

--- src/time_stepping/mo_manage_pchevi.f90
+++ src/time_stepping/mo_manage_pchevi.f90
@@ -26,14 +26,16 @@ module mo_manage_pchevi
                            temperature,wind_div,viscosity_triangles,viscosity,viscosity_rhombi, &
                            condensates_sediment_heat,molecular_diffusion_coeff,v_squared_grad, &
                            time_coordinate,vert_hor_viscosity,vector_field_placeholder, &
-                           totally_first_step_bool,gravity_m,curl_of_vorticity) &
+                           totally_first_step_bool,gravity_m,curl_of_vorticity,tke, &
+                           theta_v_pert_old) &
   bind(c,name = "manage_pchevi")
     
     real(wp), intent(out) :: wind_new(n_vectors),wind_tend(n_vectors),condensates_sediment_heat(n_scalars), &
                              wind_old(n_vectors),temperature(n_scalars),wind_div(n_scalars), &
                              viscosity_rhombi(n_vectors),viscosity(n_scalars),curl_of_vorticity(n_vectors), &
                              molecular_diffusion_coeff(n_scalars),vert_hor_viscosity(n_h_vectors+n_vectors_h), &
-                             vector_field_placeholder(n_vectors),v_squared_grad(n_vectors),v_squared(n_scalars)
+                             vector_field_placeholder(n_vectors),v_squared_grad(n_vectors),v_squared(n_scalars), &
+                             tke(n_scalars),theta_v_pert_old(n_scalars)
     integer,  intent(in)  :: adjacent_signs_h(6*n_scalars_h),adjacent_vector_indices_h(6*n_scalars_h), &
                              totally_first_step_bool,vorticity_indices_triangles(3*n_dual_scalars_h), &
                              vorticity_signs_triangles(3*n_dual_scalars_h),trsk_indices(10*n_vectors_h), &
