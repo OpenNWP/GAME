3c2470ef117b1d7a9edd4f194a22ab81fc9ba4dc
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-09-01 23:48:42 +0200

    header files removed

--- src/radiation/radiation.h
+++
@@ -1,9 +0,0 @@
-/*
-This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
-Github repository: https://github.com/OpenNWP/GAME
-*/
-
-extern int radiation_init();
-extern int calc_radiative_flux_convergence();
-extern int held_suar();
-extern int call_radiation();
