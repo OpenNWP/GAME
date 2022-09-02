9a4e9a3214b0cba5bbca554d0182fe2eeeebb1c3
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-09-03 00:10:19 +0200

    formal changes

--- run_scripts/.sh/root_script.sh
+++ run_scripts/.sh/root_script.sh
@@ -18,6 +18,6 @@ then
   exit 1
 fi
 
-$game_home_dir/build/game $run_span_min $write_out_interval_min $momentum_diff_h $momentum_diff_v $rad_on $prog_soil_temp $write_out_integrals $temperature_diff_h $start_year $start_month $start_day $start_hour $temperature_diff_v $run_id $orography_id $ideal_input_id $pressure_level_output_switch $model_level_output_switch $surface_output_switch $time_to_next_analysis $pbl_scheme $mass_diff_h $mass_diff_v $sfc_phase_trans $sfc_sensible_heat_flux
+$game_home_dir/build/game
 
 cd - > /dev/null
