57a92d949efd82062c82ea86a66131c4fb24e74b
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-09-25 13:45:11 +0200

    formal changes

--- src/spatial_operators/mo_momentum_diff_diss.f90
+++ src/spatial_operators/mo_momentum_diff_diss.f90
@@ -7,9 +7,7 @@ module mo_momentum_diff_diss
 
   use mo_definitions,        only: wp,t_grid,t_state,t_diag
   use mo_constants,          only: EPSILON_SECURITY
-  use mo_grid_nml,           only: n_scalars,n_vectors,n_cells,n_h_vectors,n_levels, &
-                                   n_dual_vectors_per_layer,n_dual_vectors,n_edges, &
-                                   n_layers,n_vectors_per_layer,n_dual_v_vectors,n_v_vectors
+  use mo_grid_nml,           only: n_cells,n_h_vectors,n_levels,n_edges,n_layers,n_v_vectors
   use mo_constituents_nml,   only: n_constituents,n_condensed_constituents
   use mo_inner_product,      only: inner_product
   use mo_divergences,        only: div_h,add_vertical_div
@@ -31,7 +29,7 @@ module mo_momentum_diff_diss
     type(t_grid),  intent(in)    :: grid  ! grid quantities
     
     ! local variables
-    integer :: h_index,layer_index,vector_index,scalar_index_from,scalar_index_to
+    integer :: h_index,layer_index,scalar_index_from,scalar_index_to
     
     ! Preparation of kinematic properties of the wind field
     ! -----------------------------------------------------
@@ -73,10 +71,9 @@ module mo_momentum_diff_diss
     call hor_calc_curl_of_vorticity(diag,grid)
     
     ! adding up the two components of the momentum diffusion acceleration and dividing by the density at the edge
-    !$omp parallel do private(h_index,layer_index,vector_index,scalar_index_from,scalar_index_to)
+    !$omp parallel do private(h_index,layer_index,scalar_index_from,scalar_index_to)
     do h_index=1,n_edges
       do layer_index=0,n_layers-1
-        vector_index = n_cells + layer_index*n_vectors_per_layer + h_index
         scalar_index_from = layer_index*n_cells + grid%from_cell(h_index)
         scalar_index_to = layer_index*n_cells + grid%to_cell(h_index)
         diag%friction_acc_h(h_index,layer_index+1) = &
@@ -98,17 +95,16 @@ module mo_momentum_diff_diss
     type(t_grid),  intent(in)    :: grid  ! grid quantities
     
     ! local variables
-    integer  :: ji,layer_index,h_index,vector_index
+    integer  :: ji,layer_index,h_index
     real(wp) :: z_upper,z_lower,delta_z
     
     ! 1.) vertical diffusion of horizontal velocity
     ! ---------------------------------------------
     ! calculating the vertical gradient of the horizontal velocity at half levels
-    !$omp parallel do private(ji,layer_index,h_index,vector_index)
+    !$omp parallel do private(ji,layer_index,h_index)
     do ji=n_edges+1,n_h_vectors+n_edges
       layer_index = (ji-1)/n_edges
       h_index = ji - layer_index*n_edges
-      vector_index = n_cells + h_index + (layer_index-1)*n_vectors_per_layer
       ! at the surface
       if (layer_index==n_layers) then
         diag%dv_hdz(ji) = state%wind_h(h_index,layer_index)/(grid%z_vector_h(h_index,layer_index) &
@@ -128,11 +124,10 @@ module mo_momentum_diff_diss
     call vert_hor_mom_viscosity(state,diag,grid)
                            
     ! now, the second derivative needs to be taken
-    !$omp parallel do private(ji,layer_index,h_index,vector_index,z_upper,z_lower,delta_z)
+    !$omp parallel do private(ji,layer_index,h_index,z_upper,z_lower,delta_z)
     do ji=1,n_h_vectors
       layer_index = (ji-1)/n_edges
       h_index = ji - layer_index*n_edges
-      vector_index = n_cells + layer_index*n_vectors_per_layer + h_index
       z_upper = 0.5_wp*(grid%z_vector_v(grid%from_cell(h_index),layer_index+1) &
       + grid%z_vector_v(grid%to_cell(h_index),layer_index+1))
       z_lower = 0.5_wp*(grid%z_vector_v(grid%from_cell(h_index),layer_index+2) &
@@ -177,10 +172,9 @@ module mo_momentum_diff_diss
     call grad_vert(diag%scalar_placeholder,diag%vector_placeholder_v,grid)
     call grad_hor(diag%scalar_placeholder,diag%vector_placeholder_h,diag%vector_placeholder_v,grid)
     ! multiplying by the already computed diffusion coefficient
-    !$omp parallel do private(h_index,layer_index,vector_index)
+    !$omp parallel do private(h_index,layer_index)
     do h_index=1,n_edges
       do layer_index=0,n_layers-1
-        vector_index = n_cells + h_index + layer_index*n_vectors_per_layer
         diag%vector_placeholder_h(h_index,layer_index+1) = 0.5_wp &
         *(diag%viscosity(grid%from_cell(h_index),layer_index+1) + diag%viscosity(grid%to_cell(h_index),layer_index+1)) &
         *diag%vector_placeholder_h(h_index,layer_index+1)
@@ -191,11 +185,10 @@ module mo_momentum_diff_diss
     ! the divergence of the diffusive flux density results in the diffusive acceleration
     call div_h(diag%vector_placeholder_h,diag%scalar_placeholder,grid)
     ! vertically averaging the divergence to half levels and dividing by the density
-    !$omp parallel do private(ji,layer_index,h_index,vector_index)
+    !$omp parallel do private(ji,layer_index,h_index)
     do ji=1,n_v_vectors-2*n_cells
       layer_index = (ji-1)/n_cells
       h_index = ji - layer_index*n_cells
-      vector_index = h_index + (layer_index+1)*n_vectors_per_layer
       ! finally adding the result
       diag%friction_acc_v(h_index,layer_index+1) = diag%friction_acc_v(h_index,layer_index+1) + 0.5_wp*( &
       diag%scalar_placeholder(h_index,layer_index+1) &
@@ -217,16 +210,14 @@ module mo_momentum_diff_diss
     type(t_grid), intent(in)    :: grid ! grid quantities
     
     ! local variables
-    integer  :: ji,jk,layer_index,h_index,vector_index,upper_index_z,lower_index_z, &
-                upper_index_zeta,lower_index_zeta,base_index
+    integer  :: ji,jk,layer_index,h_index,upper_index_zeta,lower_index_zeta
     real(wp) :: delta_z,delta_y,tangential_slope,delta_zeta,dzeta_dz,checkerboard_damping_weight
-    !$omp parallel do private(ji,jk,layer_index,h_index,vector_index,delta_z,delta_y,tangential_slope,dzeta_dz, &
-    !$omp upper_index_z,lower_index_z,upper_index_zeta,lower_index_zeta,checkerboard_damping_weight,base_index)
+    !$omp parallel do private(ji,jk,layer_index,h_index,delta_z,delta_y,tangential_slope,dzeta_dz, &
+    !$omp upper_index_zeta,lower_index_zeta,checkerboard_damping_weight)
     do ji=1,n_h_vectors
       ! Remember: (curl(zeta))*e_x = dzeta_z/dy - dzeta_y/dz = (dz*dzeta_z - dy*dzeta_y)/(dy*dz) = (dz*dzeta_z - dy*dzeta_y)/area (Stokes' Theorem, which is used here)
       layer_index = (ji-1)/n_edges
       h_index = ji - layer_index*n_edges
-      vector_index = n_cells + layer_index*n_vectors_per_layer + h_index
       diag%curl_of_vorticity_h(h_index,layer_index+1) = 0._wp
       delta_z = 0._wp
       checkerboard_damping_weight = &
@@ -234,7 +225,6 @@ module mo_momentum_diff_diss
       - diag%rel_vort_on_triangles(grid%from_cell_dual(h_index),layer_index+1)) &
       /(abs(diag%rel_vort_on_triangles(grid%to_cell_dual(h_index),layer_index+1)) &
       + abs(diag%rel_vort_on_triangles(grid%from_cell_dual(h_index),layer_index+1)) + EPSILON_SECURITY)
-      base_index = n_edges + layer_index*n_dual_vectors_per_layer
       ! horizontal difference of vertical vorticity (dzeta_z*dz)
       ! An averaging over three rhombi must be done.
       do jk=1,3
@@ -272,16 +262,12 @@ module mo_momentum_diff_diss
         tangential_slope = delta_z/delta_y
         
         ! calculating the vertical gradient of the vertical vorticity
-        upper_index_z = n_cells + (layer_index-1)*n_vectors_per_layer + h_index
-        lower_index_z = n_cells + (layer_index+1)*n_vectors_per_layer + h_index
         upper_index_zeta = layer_index-1
         lower_index_zeta = layer_index+1
         if (layer_index==0) then
-          upper_index_z = n_cells + layer_index*n_vectors_per_layer + h_index
           upper_index_zeta = layer_index
         endif
         if (layer_index==n_layers-1) then
-          lower_index_z = n_cells + layer_index*n_vectors_per_layer + h_index
           lower_index_zeta = layer_index
         endif
         
