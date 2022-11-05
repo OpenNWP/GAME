be74afd798c92bfd1f2370a78cbeca4dea07b4fe
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-11-05 17:22:48 +0100

    formal changes

--- shared/mo_various_helpers.F90
+++ shared/mo_various_helpers.F90
@@ -90,13 +90,13 @@ module mo_various_helpers
   
   subroutine nc_check(i_status)
   
-    ! This checks wether a NetCDF function threw an error.
+    ! This checks wether a netCDF function threw an error.
   
     integer, intent(in) :: i_status
 
     if(i_status/=nf90_noerr) then 
       print *, trim(nf90_strerror(i_status))
-      stop "Netcdf threw an error."
+      stop "netCDF threw an error."
     end if
     
   end subroutine nc_check
