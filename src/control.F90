26d0221ed340f173a1e618175a8fb5e94c49aa82
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-12-04 12:30:18 +0100

    formal changes

--- src/control.F90
+++ src/control.F90
@@ -13,7 +13,7 @@ program control
   use mo_run_nml,                only: run_span_min,run_nml_setup,t_init
   use mo_grid_setup,             only: eff_hor_res,radius_rescale,set_grid_properties,dtime,toa
   use mo_io_nml,                 only: n_output_steps_10m_wind,lwrite_integrals,write_out_interval_min, &
-                                       io_nml_setup,ideal_input_id,lwrite_integrals
+                                       io_nml_setup,ideal_input_id
   use mo_surface_nml,            only: nsoillays,surface_nml_setup
   use mo_rad_nml,                only: rad_config,radiation_dtime,rad_nml_setup
   use mo_diff_nml,               only: diff_nml_setup
