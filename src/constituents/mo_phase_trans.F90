0c7e9d0ef63d0b5ea592d40d988f02570d4bfde2
Max H. Balsmeier <mhbalsmeier@gmail.com>
2022-12-27 10:46:01 +0100

    minor changes regarding land fraction

--- src/constituents/mo_phase_trans.F90
+++ src/constituents/mo_phase_trans.F90
@@ -268,7 +268,7 @@ module mo_phase_trans
         if (jl==n_layers .and. lsfc_phase_trans) then
           
           ! evaporation and latent heat rates
-          if (grid%land_fraction(ji)==0._wp) then
+          if (grid%land_fraction(ji)<1._wp) then
             ! saturation pressure at surface temperature
             if (state%temperature_soil(ji,1)>=t_0) then
               saturation_pressure_sfc = saturation_pressure_over_water(state%temperature_soil(ji,1))
