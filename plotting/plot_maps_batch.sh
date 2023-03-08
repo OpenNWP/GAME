d68d2ff4d2edd9daa5c3b7c38ade329824998350
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2023-03-08 20:54:15 +0100

    maps plotted for more domains in batch mode

--- plotting/plot_maps_batch.sh
+++ plotting/plot_maps_batch.sh
@@ -13,33 +13,41 @@ start_time_since_init=${BASH_ARGV[5]} # when to begin plotting reative to the mo
 omp_num_threads=${BASH_ARGV[6]} # relevant only for OMP
 disp_shortname_list=(
 t2 gusts10 rprate sprate tcc
+t2 gusts10 rprate sprate tcc
 sst
 ) # short names according to grib as an array 
 disp_level_list=(
+2 10 0 0 0
 2 10 0 0 0
 0
 ) # levels according to grib as an array
 on_pressure_level_list=(
 0 0 0 0 0
+0 0 0 0 0
 0
 ) # set this to 1 for each plot individually if the variable resides on pressure levels
 plot_intervals_list_min=(
+$plot_interval_min $plot_interval_min $plot_interval_min $plot_interval_min $plot_interval_min
 $plot_interval_min $plot_interval_min $plot_interval_min $plot_interval_min $plot_interval_min
 0
 ) # every how many minutes you want to plot each variable
 uniform_colormap_list=(
 0 0 0 0 0
+0 0 0 0 0
 0
 ) # set this to 1 for each plot individually if you want to enforce a uniform colormap for all the time steps
 scope_list=(
 CEU CEU CEU CEU CEU
+CONUS CONUS CONUS CONUS CONUS
 WORLD
 ) # the areas of the plots
 projections_list=(
 Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic
+Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic
 EckertIII
 ) # the projections of the plots
 synoptical_time_mode=(
+1 1 1 1 1
 1 1 1 1 1
 1
 ) # this forces the time description to be of the form "init: ..., valid: ... (+ ....)"
